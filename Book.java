bd880b85fe78505a3af8845bbf1debf0b6620fb0
Markus Müller <152977588+hippo-topaz1@users.noreply.github.com>
2023-12-06 20:10:17 +0700

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,12 @@
+// Book.java
+public class Book {
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    // Getters and setters
+}
